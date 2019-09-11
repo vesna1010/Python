8be3c0ba4c8f88c01349894f8796a14d331f1a0c
Vesna <vesnasdoboj@gmail.com>
2019-09-11 22:27:30 +0200

    Update calendar.py

--- calendar/model/calendar.py
+++ calendar/model/calendar.py
@@ -7,27 +7,19 @@ class Calendar:
         self.month = month
     
     def showCalendar(self):
-        currentDay = date(year=self.year, month=self.month, day=1)
-        weekDay = currentDay.weekday();
-        
-        if  weekDay > 0:
-            delta = timedelta(days=weekDay)
-            currentDay = currentDay - delta
-     
-        print(currentDay.strftime(' %B %Y')) 
-        print('_____________________________________')  
+        currentDate = date(year=self.year, month=self.month, day=1)
+        weekDay = currentDate.weekday();
+        oneDay = timedelta(days=1)
+         
+        print(currentDate.strftime(' %B %Y')) 
+        print('_' * 35)  
         print('{:5s}{:5s}{:5s}{:5s}{:5s}{:5s}{:5s}'.format('  MON', '  TUE' , '  WED', '  THU', '  FRI', '  SAT', '  SUN')) 
-       
-        while currentDay.month < self.month or currentDay.year < self.year:
-            print('{:5s}'.format(''), end='')
+        print('{:5s}'.format('') * weekDay, end='')
+                  
+        while currentDate.month == self.month:
+            print("{:5d}".format(currentDate.day), end='')
             
-            currentDay = currentDay + timedelta(days=1)
-            
-        while currentDay.month == self.month:
-            print("{:5d}".format(currentDay.day), end='')
-            
-            if currentDay.weekday() == 6:
+            if currentDate.weekday() == 6:
                 print() 
                 
-            currentDay = currentDay + timedelta(days=1)
-            
+            currentDate = currentDate + oneDay
