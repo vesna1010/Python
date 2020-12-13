e5b1a2e58be9f2bb5fb6e480b5dabb94a1ddefb7
Vesna S <vesnasdoboj@gmail.com>
2020-12-14 00:57:49 +0100

    Update sudoku.py

--- sudokugenerator/model/sudoku.py
+++ sudokugenerator/model/sudoku.py
@@ -1,37 +1,33 @@
 from random import shuffle
 
+
 class Sudoku:
     
     def __init__(self):
         self.matrix = [[None for i in range(9)] for j in range(9)]
-        row = Sudoku.generateRow(9) 
+        row = Sudoku.generate_row_of_natural_numbers_to(9) 
         
         for k in range (0, 3):
             for i in range (k, 9, 3):
+                
                 for j in range(9):
                     self.matrix[i][j] = row[j]
                     
-                Sudoku.moveOrderElements(row)
-           
+                Sudoku.move_last_element_to_first_position(row)
+        
     @staticmethod
-    def generateRow(n):
+    def generate_row_of_natural_numbers_to(n):
         row = [i for i in range(1, (n + 1))]
         shuffle(row)
         
         return row
     
     @staticmethod
-    def moveOrderElements(elements):
-        lastElement = elements[ - 1]
-        
-        for i in range((len(elements) - 1), 0, -1):
-            elements[i] = elements[i - 1]
+    def move_last_element_to_first_position(row):
+        for i in range((len(row) - 1), 0, -1):
+            row[i], row[i - 1] = row[i - 1], row[i]
             
-        elements[0] = lastElement
-        
-        return elements
-    
-    def showMatrix(self):
+    def show_matrix(self):
         for i in range(9):
             for j in range(9):
                 if (j + 1) % 3 == 0:   
@@ -43,3 +39,4 @@ class Sudoku:
             
             if (i + 1) % 3 == 0:
                 print()
+                
