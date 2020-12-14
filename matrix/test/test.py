bd49f9eadfda5ef7f271705316e33be8f0e12792
Vesna S <vesnasdoboj@gmail.com>
2020-12-14 23:42:30 +0100

    Update test.py

--- matrix/test/test.py
+++ matrix/test/test.py
@@ -7,33 +7,38 @@ while number_of_rows_matrix1 <= 0:
 
 while number_of_columns_matrix1 <= 0:
     number_of_columns_matrix1 = int(input('Enter number of columns in first matrix: '))
-
-matrix1 = Matrix(number_of_rows=number_of_rows_matrix1, number_of_columns=number_of_columns_matrix1)    
     
 while number_of_rows_matrix2 <= 0:
     number_of_rows_matrix2 = int(input('Enter number of rows in second matrix: '))
 
 while number_of_columns_matrix2 <= 0:
-    number_of_columns_matrix2 = int(input('Enter number of columns in second matrix: '))
+    number_of_columns_matrix2 = int(input('Enter number of columns in second matrix: '))    
+    
+matrix1 = [[None for i in range(number_of_columns_matrix1)] for j in range(number_of_rows_matrix1)]
+matrix2 = [[None for i in range(number_of_columns_matrix2)] for j in range(number_of_rows_matrix2)]
 
-matrix2 = Matrix(number_of_rows=number_of_rows_matrix2, number_of_columns=number_of_columns_matrix2)  
+for i in range(number_of_rows_matrix1):
+    for j in range(number_of_columns_matrix1):
+        matrix1[i][j] = int(input('matrix1[{0}][{1}] = '.format(i, j)))
+        
+for i in range(number_of_rows_matrix2):
+    for j in range(number_of_columns_matrix2):
+        matrix2[i][j] = int(input('matrix2[{0}][{1}] = '.format(i, j)))
 
-print()
-
-try : 
-    matrix_result1 = matrix1 * matrix2
+try: 
+    matrix_result1 = Matrix(matrix1) * Matrix(matrix2)
     matrix_result1.show_matrix()
 except ValueError as e:
     print(e)
     
-    print()
+print()
     
-try :     
-    matrix_result2 = matrix1 + matrix2
+try: 
+    matrix_result2 = Matrix(matrix1) + Matrix(matrix2)
     matrix_result2.show_matrix()
 except ValueError as e:
     print(e)
-
+    
 
 
 
