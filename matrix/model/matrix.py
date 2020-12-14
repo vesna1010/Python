19a82df3197e7a509f24fc5735a036b586d4a0d5
Vesna S <vesnasdoboj@gmail.com>
2020-12-14 23:02:27 +0100

    Update matrix.py

--- matrix/model/matrix.py
+++ matrix/model/matrix.py
@@ -1,35 +1,48 @@
 class Matrix:
     
-    def __init__(self, **kwargs):
-        if kwargs.get('matrix'):
-            self.matrix = kwargs.get('matrix')
+    def __init__(self, matrix=None, number_of_rows=0, number_of_columns=0):
+        if matrix:
+            self.matrix = matrix
             
-        elif kwargs.get('rows') > 0 and kwargs.get('columns') > 0:
-            self.matrix = [[None for i in range(kwargs.get('columns'))] for j in range(kwargs.get('rows'))]
+        elif number_of_rows > 0 and number_of_columns > 0:
+            self.matrix = [[None for i in range(number_of_columns)] for j in range(number_of_rows)]
             
-            for i in range(len(self.matrix)):
-                for j in range(len(self.matrix[0])):
+            for i in range(number_of_rows):
+                for j in range(number_of_columns):
                     self.matrix[i][j] = int(input('matrix[{0}][{1}] = '.format(i, j)))
-        else:
-            raise ValueError('EFnter the number of rows and columns in the matrix!')
     
-    def __mul__(self, other):
-        matrixResult = [[None for i in range(len(other.matrix[0]))] for j in range(len(self.matrix))]
+        else:
+            raise ValueError('Enter the number of rows and columns in the matrix!')
+    
+    def __mul__(self, other_matrix):
+        matrix_result = [[None for i in range(len(other_matrix.matrix[0]))] for j in range(len(self.matrix))]
         
-        if len(self.matrix[0]) != len(other.matrix):
+        if len(self.matrix[0]) != len(other_matrix.matrix):
             raise ValueError('Cannot multiply matrices!')
          
         for i in range(len(self.matrix)):
-            for j in range(len(other.matrix[0])):
-                matrixResult[i][j] = 0
+            for j in range(len(other_matrix.matrix[0])):
+                matrix_result[i][j] = 0
                 
-                for k in range(0, len(other.matrix)):
-                    matrixResult[i][j] += self.matrix[i][k] * other.matrix[k][j]
+                for k in range(0, len(other_matrix.matrix)):
+                    matrix_result[i][j] += self.matrix[i][k] * other_matrix.matrix[k][j]
                     
-        return Matrix(matrix=matrixResult)
+        return Matrix(matrix=matrix_result)
+    
+    def __add__(self, other_matrix):
+        matrix_result = [[None for i in range(len(self.matrix[0]))] for j in range(len(self.matrix))]
         
-    def showMatrix(self):
-        for i in range (0, len(self.matrix)):
+        if len(self.matrix) != len(other_matrix.matrix) or len(self.matrix[0]) != len(other_matrix.matrix[0]):
+            raise ValueError('Cannot add matrices!')
+         
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                matrix_result[i][j] = self.matrix[i][j] + other_matrix.matrix[i][j]
+                    
+        return Matrix(matrix=matrix_result)
+    
+    def show_matrix(self):
+        for i in range (len(self.matrix)):
             for j in range(len(self.matrix[i])):
                 print('{:5d}'.format(self.matrix[i][j]), end='')
             print() 
