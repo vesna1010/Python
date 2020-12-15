7b2f94409d58c812d0155ae8787095e7726bfee6
Vesna S <vesnasdoboj@gmail.com>
2020-12-15 23:08:09 +0100

    Update card.py

--- bingocard/model/card.py
+++ bingocard/model/card.py
@@ -2,23 +2,23 @@ from random import Random
 
 
 class Card: 
+    random = Random()
 
     def __init__(self):
-        self.card_numbers = {}
         numbers = [n for n in range(1, 91)]
+        self.card_numbers = [[0 for i in range(9)] for j in range(9)]
         
         for i in range(9):
             for key, value in Card.select_five_numbers_of(numbers).items():
-                self.card_numbers[str(i) + str(key)] = value
+                self.card_numbers[i][key] = value
                 numbers.remove(value)
         
     @staticmethod
     def select_five_numbers_of(numbers):
-        random = Random()
         selected_numbers = {}
         
         while len(selected_numbers) < 5:
-            index = random.randint(0, len(numbers) - 1)
+            index = Card.random.randint(0, len(numbers) - 1)
             number = numbers[index]
             
             if number == 90:
@@ -31,10 +31,13 @@ class Card:
     def print(self):
         for i in range(9):
             for j in range(9):
-                key = str(i) + str(j)
-                print('{:>5s}|'.format(str(self.card_numbers.get(key, ''))), end='')
+                if self.card_numbers[i][j]:
+                    print('{:>5d}|'.format(self.card_numbers[i][j]), end='')
+                else:
+                    print('{:>5s}|'.format(''), end='')
             print()
                 
             if (i + 1) % 3 == 0:
                 print()       
+
                 
